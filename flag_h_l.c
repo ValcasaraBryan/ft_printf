7c97013993e8ac96aa8068f1eaab4192b54a95b8
ValcasaraBryan <bryanvalcasara@live.fr>
2018-07-04 02:36:11 +0200

    ajout de la priorite des flag

--- flag_h_l.c
+++ flag_h_l.c
@@ -14,9 +14,9 @@
 
 char	*flag(char *res, int i, t_tab *list, int *z)
 {
-	if (valid_flag_short(z) && list->c == 'd')
+	if (!valid_flag_long(z) && !valid_flag_long_long(z) &&  valid_flag_short(z) && list->c == 'd')
 		flag_string(res, i, ft_lltoa((short int)list->f), z);
-	else if (valid_flag_short_short(z) && list->c == 'd')
+	else if (!valid_flag_long(z) && !valid_flag_long_long(z) && valid_flag_short_short(z) && list->c == 'd')
 		flag_string(res, i, ft_lltoa((char)list->f), z);
 	else if (valid_flag_long(z) && list->c == 'd')
 		flag_string(res, i, ft_lltoa((long)list->f), z);
@@ -29,9 +29,9 @@ char	*flag(char *res, int i, t_tab *list, int *z)
 
 char	*flag_u(char *res, int i, t_tab *list, int *z)
 {
-	if (valid_flag_short(z) && list->c == 'u')
+	if (!valid_flag_long(z) && !valid_flag_long_long(z) && valid_flag_short(z) && list->c == 'u')
 		flag_u_string(res, i, ft_lltoa((unsigned short int)list->f), z);
-	else if (valid_flag_short_short(z) && list->c == 'u')
+	else if (!valid_flag_long(z) && !valid_flag_long_long(z) && valid_flag_short_short(z) && list->c == 'u')
 		flag_string(res, i, ft_lltoa((unsigned char)list->f), z);
 	else if (valid_flag_long(z) && list->c == 'u')
 		flag_string(res, i, ft_ulltoa((unsigned long)list->f), z);
